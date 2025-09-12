0a3e6a4632a116f1821b09f0b02baca4c60d2b2a
WesleyMota02 <wesleymota600@gmail.com>
2025-09-12 17:05:52 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,9 @@
+
+const email = document.getElementById('txt').value;
+const password = document.getElementById('Senha').value;
+
+
+if (email === '' || password === '') {
+    alert('Por favor, preencha todos os campos!');
+
+}
